2b133b29cb6b74b1d713deb7bb2772426f82d366
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2021-11-21 23:59:42 -0300

    Add files via upload

---
+++ regressao_simples.R
@@ -0,0 +1,13 @@
+
+# Regressão linear simples -----------------------------------------------------------------------------------------------------------------
+
+
+# Carregando pacotes necessários -----------------------------------------------------------------------------------------------------------
+
+library(tibble) # Pacote para ler tabela simplificada
+library(dplyr) # Pacote para calcular a média, desvios, etc.
+
+# Carregando conjunto de dados -------------------------------------------------------------------------------------------------------------
+
+datasets::trees # Escolher banco de dados disponível no R
+tibble(trees) # Carrega bando de dados
