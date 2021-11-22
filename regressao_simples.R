e3b4fac160456d17df241cd6d11204ae6a4dd700
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2021-11-22 00:10:26 -0300

    Add files via upload

--- regressao_simples.R
+++ regressao_simples.R
@@ -6,8 +6,25 @@
 
 library(tibble) # Pacote para ler tabela simplificada
 library(dplyr) # Pacote para calcular a média, desvios, etc.
+library(ggplot2) # Pacote para produzir os gráficos
 
 # Carregando conjunto de dados -------------------------------------------------------------------------------------------------------------
 
 datasets::trees # Escolher banco de dados disponível no R
 tibble(trees) # Carrega bando de dados
+
+# Descrição dos dados ----------------------------------------------------------------------------------------------------------------------
+
+dados <- trees
+
+dados %>%
+  summarise(mean(Girth), mean(Height), mean(Volume),
+            sd(Girth), sd(Height), sd(Volume),
+            seG = sd(Girth) / sqrt(length(Girth)),
+            seH = sd(Height) / sqrt(length(Height)),
+            seV = sd(Volume) / sqrt(length(Volume)))
+
+
+# Visualizando as correlações --------------------------------------------------------------------------------------------------------------
+
+
